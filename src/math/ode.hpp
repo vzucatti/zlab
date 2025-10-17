174c5b22065aee0f74bbd018d65da5aefea8792b
Victor Zucatti <victor.zucatti@gmail.com>
2025-10-17 17:12:53 -0400

    added ButcherTableau struct

--- src/math/ode.hpp
+++ src/math/ode.hpp
@@ -1,8 +1,18 @@
 
 #pragma one
 
+#include <array>
+
+#include "core.hpp"
+
 namespace zlab{
 
+template <positiveIntegerType numberOfStages>
+struct ButcherTableau {
+    std::array<std::array<scalarType, numberOfStages>, numberOfStages> A;
+    std::array<scalarType, numberOfStages> b;
+    std::array<scalarType, numberOfStages> c;
+};
 
 
 } // end namespace zlab
