1511fad40f149f02cefa7e40c4278043ebae0be7
Victor Zucatti <victor.zucatti@gmail.com>
2025-10-21 02:54:37 -0400

    refactor: remove unnecessary this->

--- src/math/rbf.cpp
+++ src/math/rbf.cpp
@@ -4,21 +4,21 @@
 namespace zlab{
 
 scalarType Bump::evaluate(scalarType r) const {
-    auto R = this->radius;
+    auto R = radius;
     if (r > R) return 0;
     auto ratio = r / R;
     return std::exp(-1 / (1 - zlab::pow(ratio,2)));
 }
 
 scalarType WendlandC0::evaluate(scalarType r) const {
-    auto R = this->radius;
+    auto R = radius;
     if(r > R) return 0;
     auto ratio = r / R;
     return zlab::pow(1 - ratio, 2);
 }
 
 scalarType WendlandC2::evaluate(scalarType r) const {
-    auto R = this->radius;
+    auto R = radius;
     if(r > R) return 0;
     auto ratio = r / R;
     return zlab::pow(1 - ratio, 4) * (4 * ratio + 1);
