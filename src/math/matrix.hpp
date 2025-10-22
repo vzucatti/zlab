0f202ff9f7a9822d0be2393488fa97fc5e8e74c9
Victor Zucatti <victor.zucatti@gmail.com>
2025-10-22 01:50:19 -0400

    feature: aypx takes vectors of different types

--- src/math/matrix.hpp
+++ src/math/matrix.hpp
@@ -112,9 +112,8 @@ void axpby(scalarType a, const vectorTypeX& x, scalarType b, vectorTypeY& y){
     }
 }
 
-template <typename vectorType>
-requires VectorConcept<vectorType>
-void aypx(scalarType a, vectorType& y, const vectorType& x){
+template <VectorConcept vectorTypeX, VectorConcept vectorTypeY>
+void aypx(scalarType a, vectorTypeX& y, const vectorTypeY& x){
     assert(x.length() == y.length());
     for(auto i=0; i < x.length(); i++){
         y[i] = a * y[i] + x[i];
